847ed9f33349449fb7a798b357cceb7e483a9f18
joelyoung12 <joelbarnabas589@gmail.com>
2022-05-11 12:54:27 +0100

    new commit

--- new.js
+++ new.js
@@ -1,10 +1,21 @@
 window.addEventListener('scroll', function(){
     var nav = this.document.querySelector('nav');
     nav.classList.toggle('sticky', window.scrollY > 0)
+
+   $('.dropdown li a').css('color', 'white')
 });
 $(document).ready( function(){
     $('.icon-bar').click( function(){
         $('nav').toggle()
     })
+    $('.serDown').click( function(){
+        $('.dropdown').toggle()
+    })
+    $('#times').click( function(){
+        $('.dropdown').hide()
+    })
+    
 })
+
+
 
